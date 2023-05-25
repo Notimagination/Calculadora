24ec0424da6696e76122b436699b381ef671d19d
Notimagination <68918458+Notimagination@users.noreply.github.com>
2023-05-25 20:03:21 -0300

    Update Menu.java
    Added sounds to buttons
    Changed background and main images

--- Menu.java
+++ Menu.java
@@ -1,14 +1,42 @@
 import java.awt.Color;
 import java.awt.Desktop;
 import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.AudioSystem;
 
 public class Menu {
+
+    public static void playSound(String soundName) {
+        
+        try 
+        
+        {
+            
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile( ));
+            Clip clip = AudioSystem.getClip( );
+            clip.open(audioInputStream);
+            clip.start( );
+        
+        }
+        
+        catch(Exception ex)
+        
+        {
+            
+            ex.printStackTrace( );
+        
+        }
+    
+    }
 
     public static void menuPrincipal() {
 
@@ -65,13 +93,13 @@ public class Menu {
         botonGit.setFocusPainted(false);
         botonGit.setContentAreaFilled(false);
 
-        JButton texto = new JButton("Versión 1.0");
+        JButton texto = new JButton("Versión 1.2");
 
         texto.setBorderPainted(false);
         texto.setFocusPainted(false);
         texto.setContentAreaFilled(false);
         texto.setFont(new Font("Texto", Font.ITALIC, 17));
-        texto.setForeground(Color.YELLOW);
+        texto.setForeground(Color.CYAN);
 
         botonSuma.setBounds(90,30,30,30);
         botonResta.setBounds(165,30,30,30);
@@ -95,6 +123,7 @@ public class Menu {
         }
 
         //menuPrincipal.getContentPane().setBackground(Color.CYAN);
+        
         menuPrincipal.add(botonSuma);
         menuPrincipal.add(botonResta);
         menuPrincipal.add(botonMultip);
@@ -107,12 +136,14 @@ public class Menu {
         menuPrincipal.setLocationRelativeTo(null);
         menuPrincipal.setLayout(null);
         menuPrincipal.setVisible(true);
-
         menuPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuPrincipal.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\User\\Desktop\\Calculadora\\recursos\\main.png"));
 
         botonSuma.addActionListener(new ActionListener() {  
             
             public void actionPerformed(ActionEvent e){
+
+                playSound("C:\\Users\\User\\Desktop\\Calculadora\\sonidos\\clic_2.wav");
 
                 Suma.suma();
 
@@ -123,6 +154,8 @@ public class Menu {
         botonResta.addActionListener(new ActionListener() {
             
             public void actionPerformed(ActionEvent e){
+
+                playSound("C:\\Users\\User\\Desktop\\Calculadora\\sonidos\\clic_2.wav");
                 
                 Resta.resta();
             
@@ -134,6 +167,8 @@ public class Menu {
             
             public void actionPerformed(ActionEvent e){
 
+                playSound("C:\\Users\\User\\Desktop\\Calculadora\\sonidos\\clic_2.wav");
+
                 Multiplicacion.multiplicacion();
             
             }
@@ -143,6 +178,8 @@ public class Menu {
         botonDiv.addActionListener(new ActionListener() {
             
             public void actionPerformed(ActionEvent e){
+
+                playSound("C:\\Users\\User\\Desktop\\Calculadora\\sonidos\\clic_2.wav");
 
                 Division.divion();
 
@@ -154,6 +191,8 @@ public class Menu {
             
             public void actionPerformed(ActionEvent e){
 
+                playSound("C:\\Users\\User\\Desktop\\Calculadora\\sonidos\\clic.wav");
+
                 Historial.historial();
 
             }
@@ -164,6 +203,18 @@ public class Menu {
             
             public void actionPerformed(ActionEvent e){
 
+                playSound("C:\\Users\\User\\Desktop\\Calculadora\\sonidos\\close.wav");
+
+                try {
+                    
+                    TimeUnit.SECONDS.sleep(1);
+
+                } catch (InterruptedException e1) {
+
+                    e1.printStackTrace();
+
+                }
+    
                 System.exit(0);
 
             }
@@ -174,9 +225,11 @@ public class Menu {
             
             public void actionPerformed(ActionEvent e){
 
+                playSound("C:\\Users\\User\\Desktop\\Calculadora\\sonidos\\clic.wav");
+
                 try {
 
-                    Desktop.getDesktop().browse(new URL("https://github.com/Notimagination?tab=repositories").toURI());
+                    Desktop.getDesktop().browse(new URL("https://github.com/Notimagination/Calculadora").toURI());
 
                 } catch (Exception error) {}
 
